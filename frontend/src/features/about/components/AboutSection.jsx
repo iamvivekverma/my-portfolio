9407248b6f12f5d1c51c0395c19711e57707f67d
vivek kumar <iamvivek.verma@icloud.com>
2026-09-02 12:19:16 +0530

    Update image source in AboutSection component

--- frontend/src/features/about/components/AboutSection.jsx
+++ frontend/src/features/about/components/AboutSection.jsx
@@ -61,7 +61,7 @@ export default function AboutSection() {
           
           <div className="lg:col-span-8 overflow-hidden rounded-2xl lg:h-full">
             <img
-              src="/assets/images/vi.svg"
+              src="/assets/images/vivek.svg"
               alt="Vivek"
               className="w-full h-full object-cover object-[50%_20%]"
             />
